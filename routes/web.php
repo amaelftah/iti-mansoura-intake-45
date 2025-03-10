c329230cf46d5399862d15c348701956afe94bba
Ahmed Mohamed Abd El Ftah <ahmedabdelftah95165@gmail.com>
2025-03-10 11:09:14 +0200

    Add authentication middleware to post routes

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,13 @@ use Inertia\Inertia;
 use App\Http\Controllers\PostController;
 use App\Models\Post;
 
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::middleware(['auth'])->get('/posts', [PostController::class, 'index'])->name('posts.index');
 
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+Route::middleware(['auth'])->get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 
-Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+Route::middleware(['auth'])->get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 
-Route::post('posts', [PostController::class, 'store'])->name('posts.store');
+Route::middleware(['auth'])->post('posts', [PostController::class, 'store'])->name('posts.store');
 
 
 Route::get('/', function () {
